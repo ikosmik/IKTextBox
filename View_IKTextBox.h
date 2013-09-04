ed541f3cf1e65134f78b755a38c0b9c38a338f4f
Priyanka <Priyanka@Priyankas-iMac.local>
2013-09-04 18:42:11 +0530

    first commit

--- View_IKTextBox.h
+++ View_IKTextBox.h
@@ -1,13 +1,29 @@
 //
 //  View_IKTextBox.h
-//  IKTextBox
+//  connecttosqlserver
 //
-//  Created by Priyanka on 9/4/13.
-//
+//  Created by Priyanka on 8/13/13.
+//  Copyright (c) 2013 iKosmik. MIT License.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface View_IKTextBox : UIViewController
+@protocol IKTextBoxDelegate <NSObject>
+-(void) doneWithTextEntry : (NSString *) text;
+@end
+
+
+@interface View_IKTextBox : UIViewController <UITextViewDelegate> {
+    BOOL isTextPlaceholder;
+}
+
+@property (nonatomic, strong) IBOutlet UITextView *txtView;
+@property (nonatomic, strong) IBOutlet UIButton *buttonDone;
+@property (nonatomic, weak) id <IKTextBoxDelegate> delegate;
+
+-(void) initializeWithText : (NSString *) text
+         IsTextPlaceholder : (BOOL) isItPlaceHolder
+                WithSecure : (BOOL) isItSecure;
+-(IBAction) buttonDonePressed : (id)sender;
 
 @end
--- View_IKTextBox.m
+++ View_IKTextBox.m
@@ -1,18 +1,80 @@
 //
 //  View_IKTextBox.m
-//  IKTextBox
+//  connecttosqlserver
 //
-//  Created by Priyanka on 9/4/13.
-//
+//  Created by Priyanka on 8/13/13.
+//  Copyright (c) 2013 ikosmik. MIT License.
 //
 
 #import "View_IKTextBox.h"
 
-@interface View_IKTextBox ()
-
-@end
 
 @implementation View_IKTextBox
+
+@synthesize delegate;
+
+
+
+#pragma mark - Initializer
+
+/**
+ * Initalizes the page
+ * 1. Clears the text in the textview
+ * 2. Makes the keyboard come up (first responder)
+ *
+ * @created 13 Aug 2013
+ */
+-(void) initializeWithText : (NSString *)text
+         IsTextPlaceholder : (BOOL)isItPlaceHolder
+                WithSecure : (BOOL)isItSecure {
+    
+    NSLog(@"initializeWithText secure %d", isItSecure);
+    [self.txtView setSecureTextEntry : isItSecure];
+    isTextPlaceholder = isItPlaceHolder;
+    if (isTextPlaceholder) {
+        [self.txtView setTextColor:[UIColor lightGrayColor]];
+    }
+    [self.txtView setText:text];
+    
+    //[self.txtView becomeFirstResponder];
+}
+
+
+
+#pragma mark Delegate Handler
+
+- (void)notifyDelegateAboutTextEntryCompletion {
+    NSLog(@"notifyDelegateAboutTextEntryCompletion");
+    if ([self.delegate respondsToSelector:@selector(doneWithTextEntry:)]) {
+        [self.delegate doneWithTextEntry : self.txtView.text];
+    }
+}
+
+
+
+#pragma mark - Button Handler
+
+-(IBAction) buttonDonePressed : (id)sender {
+    [self.view removeFromSuperview];
+    [self notifyDelegateAboutTextEntryCompletion];
+    [self.txtView resignFirstResponder];
+    [self.txtView setText:kEmptyString];
+}
+
+
+
+#pragma mark - TextViewDelegate Handler
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    NSLog(@"textViewDidBeginEditing");
+    if (isTextPlaceholder) {
+        [self.txtView setText:kEmptyString];
+    }
+    [self.txtView setTextColor:[UIColor blackColor]];
+}
+
+
+#pragma mark - Generic Class Methods
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -23,9 +85,12 @@
     return self;
 }
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self.txtView setDelegate:self];
 	// Do any additional setup after loading the view.
 }
 
