3e40f040a729831830be2b24f9dc1b1da1f03cd0
Priyanka <Priyanka@Priyankas-iMac.local>
2013-09-04 18:35:19 +0530

    first commit

---
+++ View_IKTextBox.h
@@ -0,0 +1,13 @@
+//
+//  View_IKTextBox.h
+//  IKTextBox
+//
+//  Created by Priyanka on 9/4/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View_IKTextBox : UIViewController
+
+@end
---
+++ View_IKTextBox.m
@@ -0,0 +1,38 @@
+//
+//  View_IKTextBox.m
+//  IKTextBox
+//
+//  Created by Priyanka on 9/4/13.
+//
+//
+
+#import "View_IKTextBox.h"
+
+@interface View_IKTextBox ()
+
+@end
+
+@implementation View_IKTextBox
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
